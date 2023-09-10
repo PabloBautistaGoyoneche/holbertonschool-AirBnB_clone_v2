aedbb96d02d07898d2960a4204af8f11f9aab1ae
PabloBautistaGoyoneche <pablo.bautista.mkt@gmail.com>
2023-09-09 20:15:47 -0500

    DBStorage - User

--- models/user.py
+++ models/user.py
@@ -1,11 +1,21 @@
 #!/usr/bin/python3
-"""This module defines a class User"""
-from models.base_model import BaseModel
+from sqlalchemy import Column, String
+from models.base_model import BaseModel, Base
 
-
-class User(BaseModel):
+class User(BaseModel, Base):
     """This class defines a user by various attributes"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+
+    """ Table name in the database """
+    __tablename__ = 'users'
+
+    """ Column for email (up to 128 characters, not nullable) """
+    email = Column(String(128), nullable=False)
+
+    """ Column for password (up to 128 characters, not nullable) """
+    password = Column(String(128), nullable=False)
+
+    """ Column for first name (up to 128 characters, nullable) """
+    first_name = Column(String(128), nullable=True)
+
+    """ Column for last name (up to 128 characters, nullable) """
+    last_name = Column(String(128), nullable=True)
