dca3e0f5f4dc59ff05b0fdbda90e1bce6e7397a8
yelsy <yelsycg17@gmail.com>
2023-09-09 22:02:57 -0500

    feat: update state class

--- models/state.py
+++ models/state.py
@@ -1,8 +1,28 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+import models
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship, backref
+from models.city import City
+from os import getenv
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = "states"
+
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", cascade="all, delete", backref="state")
+    else:
+        name = ""
+
+        @property
+        def cities(self):
+            """ Return the list of cities"""
+            listCities = []
+            for city in models.storage.all(City).values():
+                if city.state_id == self.id:
+                    listCities.append(city)
+            return listCities
