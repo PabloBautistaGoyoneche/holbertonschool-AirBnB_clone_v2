27b71dc3df3ac76c73ad046e92e728a0b82ec14a
yelsy <yelsycg17@gmail.com>
2023-09-10 21:12:39 -0500

    fix: error in method do all fixing

--- console.py
+++ console.py
@@ -239,20 +239,20 @@ class HBNBCommand(cmd.Cmd):
     def do_all(self, args):
         """ Shows all objects, or all objects of a class"""
         print_list = []
-        if os.getenv('HBNB_TY_STORAGE') == 'db':
+        if os.getenv('HBNB_TYPE_STORAGE') == 'db':
             storage = storage.all(eval(args))
         else:
-            storage = storage._fileStorage_objects
+            storage = storage._fileStorage__objects
         if args:
             args = args.split(' ')[0]  # remove possible trailing args
             if args not in HBNBCommand.classes:
                 print("** class doesn't exist **")
                 return
-            for k, v in storage._FileStorage__objects.items():
+            for k, v in storage.items():
                 if k.split('.')[0] == args:
                     print_list.append(str(v))
         else:
-            for k, v in storage._FileStorage__objects.items():
+            for k, v in storage.all().items():
                 print_list.append(str(v))
 
         print(print_list)
