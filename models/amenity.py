6b564c97f17c9448d835ef4fa96a282d9f813ad8
PabloBautistaGoyoneche <pablo.bautista.mkt@gmail.com>
2023-09-11 22:30:15 -0500

    We updated the Amenity class to store service information

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,18 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel
+""" Amenity Module for HBNB project """
+from os import getenv
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from models.base_model import BaseModel, Base
 
+class Amenity(BaseModel, Base):
+    """ Amenity class to store Amenity information """
+    __tablename__ = 'amenities'
 
-class Amenity(BaseModel):
-    name = ""
+    name = Column(String(128), nullable=False)
+
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        place_amenities = relationship(
+            "Place",
+            secondary="place_amenity"
+        )
