d2fb030215dbb809ad7c607c2334bacf3ed348a9
yelsy <yelsycg17@gmail.com>
2023-09-15 21:17:30 -0500

    feat: update colum

--- models/amenity.py
+++ models/amenity.py
@@ -14,5 +14,6 @@ class Amenity(BaseModel, Base):
     if getenv('HBNB_TYPE_STORAGE') == 'db':
         place_amenities = relationship(
             "Place",
-            secondary="place_amenity"
+            secondary="place_amenity",
+            back_populates = "amenities"
         )
