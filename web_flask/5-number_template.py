4cf81c96bb03c886a685d4e2607e7bd65a7b1f22
PabloBautistaGoyoneche <pablo.bautista.mkt@gmail.com>
2023-09-17 04:35:48 -0500

    Flask to create a web application with six different routes

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""Script that starts a Flask web with 6 routes"""
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_route():
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hi_route():
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_route(text):
+    text = text.replace("_", " ")
+    return f'C {text}'
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_route(text="is_cool"):
+    text = text.replace("_", " ")
+    return f'Python {text}'
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number_route(n):
+    if isinstance(n, int):
+        return f'{n} is a number'
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def html_route(n):
+    if isinstance(n, int):
+        return render_template('5-number.html', number=n)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
